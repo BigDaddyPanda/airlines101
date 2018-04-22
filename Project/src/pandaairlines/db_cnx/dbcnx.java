0d204da39c3c633f11cff74992a607031d8aa8a0
PANDA-PC <yassinkisrawi42@gmail.com>
2018-04-22 14:58:01 +0100

    db_cnx proto

--- Project/src/pandaairlines/db_cnx/dbcnx.java
+++ Project/src/pandaairlines/db_cnx/dbcnx.java
@@ -9,6 +9,54 @@ package pandaairlines.db_cnx;
  *
  * @author ky94
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class dbcnx {
+
+    static public Connection conn = null;
+    private static String url;
+    private static String driver;
+    private static String user;
+    private static String password;
+
+    static private void connecter() {
+        url = "jdbc:mysql://localhost:3306/formationdatabase";
+        driver = "com.mysql.jdbc.Driver";
+        user = "root";
+        password = "";
+        try {
+            Class.forName(driver);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("erreur driver");
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    static public void deconnecter() {
+        try {
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    public void connect(){
+        if(conn==null) connecter();
+    }
     
 }
