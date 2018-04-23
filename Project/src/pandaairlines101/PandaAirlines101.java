5050b108f8f034e22e67536b4f8592387c4e2da2
AMINAYED-PC\amin ayed <amin.ayed87@gmail.com>
2018-04-23 16:07:35 +0100

    interface maj

--- Project/src/pandaairlines101/PandaAirlines101.java
+++ Project/src/pandaairlines101/PandaAirlines101.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pandaairlines101;
+package src.pandaairlines101;
 
-import javafx.application.Application;
+import javafx.application.Application; 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
